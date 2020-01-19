4f74f1fcc01cb189708f2c695197fde9c689693e
rhlmalik17 <rhlmalik17@gmail.com>
2020-01-20 00:50:47 +0530

    Updates

---
+++ precache-manifest.c8248a304536fea30a7462004e33591d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fa6cb854b28cba1a613f6dbbef982d",
+    "url": "/Star-Wars-Universe/index.html"
+  },
+  {
+    "revision": "cf3cf0c9b574176e9e82",
+    "url": "/Star-Wars-Universe/static/css/main.6e06c00b.chunk.css"
+  },
+  {
+    "revision": "c6eef0c3d4abf6ded5f8",
+    "url": "/Star-Wars-Universe/static/js/2.4a3f703e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Star-Wars-Universe/static/js/2.4a3f703e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf3cf0c9b574176e9e82",
+    "url": "/Star-Wars-Universe/static/js/main.f2658cbc.chunk.js"
+  },
+  {
+    "revision": "e587487ed7cd9464672d",
+    "url": "/Star-Wars-Universe/static/js/runtime-main.402cd693.js"
+  },
+  {
+    "revision": "1295343e3e8e4f07f766b72ef593490c",
+    "url": "/Star-Wars-Universe/static/media/DarthVader.1295343e.png"
+  },
+  {
+    "revision": "47ad59044794fe4531e74445d2628039",
+    "url": "/Star-Wars-Universe/static/media/Yoda.47ad5904.png"
+  }
+]);
